3d79b5b01725235f85522bc0d52008a1d67d8aec
Greg Meece <greg.meece@snapdocs.com>
2020-11-18 12:19:02 -0600

    Fixed a few failures; cleaned up formatting

--- cypress/integration/notary-search-page.spec.js
+++ cypress/integration/notary-search-page.spec.js
@@ -1,42 +1,39 @@
 /// <reference types="cypress" />
 
-import { 
-  acceptCookies
-} from "../page-objects/all-pages"
+import { acceptCookies } from "../page-objects/all-pages";
 
-import {
-  notaryCardItem
-} from "../page-objects/notary-search-page"
+import { notaryCardItem } from "../page-objects/notary-search-page";
 
-describe('Search for Notary by Zip Code', () => {
+describe("Search for Notary by Zip Code", () => {
   beforeEach(() => {
-    cy.visit('/notary-public/')
+    cy.visit("/notary-public/");
     // What, no cookies? I'm disappointed!
-  })
+  });
 
-  it('should show GPDR cookie notice', () => {
-    // Yes, this is a negative test case. 
-    // Every other main entry point shows the GPDR cookie notice
-    cy.get('#cn-accept-cookie')
-  })
+  it("Should show GPDR cookie notice", () => {
+    // Negative test case: every other main entry point shows the GPDR cookie notice
+    cy.log("Where is the GPDR Cookie Notice?");
+    cy.get("#cn-accept-cookie"); // no 'Accept Cookies' alert
+  });
 
-  it('Searching by 78665 returns results', () => {
-    cy.url().should('include', 'notary-public')
-    cy.get('#autocomplete').type('78665 {enter}')
-    cy.url().should('include', 'search?')
-    cy.get('table > tbody > tr').should('to.have.lengthOf.above', 0)
-  })
+  it("Searching by 78665 returns results", () => {
+    cy.url().should("include", "notary-public");
+    cy.get("#autocomplete").type("78665 {enter}");
+    cy.url().should("include", "search?");
+    cy.get("table > tbody > tr").should("to.have.lengthOf.above", 0);
+  });
 
-  it('Read more leads to correct Bio', () => {
-    cy.get('#autocomplete').type('78665 {enter}')
-    cy.url().should('include', 'search?')
+  it("Read more leads to correct Bio", () => {
+    cy.get("#autocomplete").type("78665 {enter}");
+    cy.url().should("include", "search?");
     cy.get(notaryCardItem(1)).then(($notaryName1) => {
-      const notaryName = $notaryName1.text().trim()
-      cy.get(':nth-child(1) > td > .media > .media-body > .notary-bio > a').click()
-      cy.get('.notary-name').should(($notaryName2) => {
-        expect($notaryName2.text()).to.eq(notaryName)
-      })
-    })
-  })
-
-})
+      const notaryName = $notaryName1.text().trim();
+      cy.get(
+        ":nth-child(1) > td > .media > .media-body > .notary-bio > a"
+      ).click();
+      cy.get(".notary-name").should(($notaryName2) => {
+        expect($notaryName2.text()).to.eq(notaryName);
+      });
+    });
+  });
+});
